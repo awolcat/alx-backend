1972da6f8ec96dd6fb006ece3fcfcc2d90be75a7
Albert <albertirura11@gmail.com>
2024-01-01 10:48:30 +0300

    hypermedia

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Module defines a class Server that mocks an API endpoint"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Mock of an API endpoint that returns a page based on
+        page number and page size parameters
+        """
+        index_range = __import__('0-simple_helper_function').index_range
+
+        assert type(page) is int and type(page_size) is int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        try:
+            result = self.dataset()[start: end]
+        except IndexError:
+            result = []
+        finally:
+            return result
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Similar to get_page() above 
+        but returns hypermedia(references to some info?)
+        """
+        hyper = {'page_size': 0, 'page': 0, 'data': [], 'next_page': 0,
+                 'prev_page': 0, 'total_pages': 0}
+        page_data = self.get_page(page, page_size)
+        hyper['page_size'] = len(page_data)
+        hyper['page'] = page
+        hyper['data'] = page_data
+        hyper['prev_page'] = page - 1 if page >= 2 else None
+        hyper['next_page'] = page + 1 if page_size * page < len(self.dataset()) else None
+        hyper['total_pages'] = math.ceil(len(self.dataset()) / page_size)
+        return hyper
+
