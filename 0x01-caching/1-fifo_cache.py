ebd3b1a29651e864de4d8f6102a9b03d531a36f7
Albert <albertirura11@gmail.com>
2024-01-03 17:51:37 +0300

    added tracking for cache items datetime info

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """BasicCache module"""
-
+from datetime import datetime
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -10,18 +10,26 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Intitalize super and self"""
         super().__init__()
+        self.keyMeta = {}
 
     def put(self, key, item):
         """Add item to cache with key key"""
         if key is None or item is None:
             return
         if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            # Delete oldest key, value pair
             cache_keys = list(self.cache_data.keys())
-            first_in_key = cache_keys[0]
+            time = datetime.now()
+            for kc in cache_keys:
+                if self.keyMeta[kc] < time:
+                    time = self.keyMeta[kc]
+                    first_in_key = kc
+            # first_in_key = self.newKeys[0]
             if key not in cache_keys:
                 self.cache_data.pop(first_in_key)
                 print(f"DICARD {first_in_key}")
         self.cache_data.update({key: item})
+        self.keyMeta.update({key: datetime.now()})
 
     def get(self, key):
         """Return the value in cache linked to key"""
