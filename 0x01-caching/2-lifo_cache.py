fd5f75735b60ffd81d8eb31162d0653a54e17234
Albert <albertirura11@gmail.com>
2024-01-03 17:24:45 +0300

    hot fix

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,13 +15,14 @@ class LIFOCache(BaseCaching):
         """Add item to cache with key key"""
         if key is None or item is None:
             return
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-            cache_keys = list(self.cache_data.keys())
-            last_in_key = cache_keys[-1]
-            if key not in cache_keys:
-                self.cache_data.pop(last_in_key)
-                print(f"DICARD {last_in_key}")
-        self.cache_data.update({key: item})
+        else:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                cache_keys = list(self.cache_data.keys())
+                last_in_key = cache_keys[-1]
+                if key not in cache_keys:
+                    self.cache_data.pop(last_in_key)
+                    print(f"DICARD {last_in_key}")
+            self.cache_data.update({key: item})
 
     def get(self, key):
         """Return the value in cache linked to key"""
