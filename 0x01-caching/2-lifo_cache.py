7f9dbe5355465c72dfc14cbded0563b8d3bc0948
Albert <albertirura11@gmail.com>
2024-01-03 18:08:36 +0300

    track datetime data for cache items

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """BasicCache module"""
-
+from datetime import datetime
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -10,19 +10,26 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Intitalize super and self"""
         super().__init__()
+        self.keyMeta = {}
 
     def put(self, key, item):
         """Add item to cache with key key"""
         if key is None or item is None:
-            return
+            pass
         else:
             if len(self.cache_data) == BaseCaching.MAX_ITEMS:
                 cache_keys = list(self.cache_data.keys())
-                last_in_key = cache_keys[-1]
+                time = self.keyMeta[cache_keys[0]]
+                for kc in cache_keys:
+                    if self.keyMeta[kc] > time:
+                        time = self.keyMeta[kc]
+                        last_in_key = kc
+                # last_in_key = cache_keys[-1]
                 if key not in cache_keys:
                     self.cache_data.pop(last_in_key)
                     print(f"DICARD {last_in_key}")
             self.cache_data.update({key: item})
+            self.keyMeta.update({key: datetime.now()})
 
     def get(self, key):
         """Return the value in cache linked to key"""
