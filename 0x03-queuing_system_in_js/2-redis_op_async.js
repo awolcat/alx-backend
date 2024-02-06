60e7ad3be0d96566d0afd322941d4a5dd209ddff
Albert <albertirura11@gmail.com>
2024-02-06 16:57:34 +0300

    async redis ops

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import { promisify } from 'util';
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.SET(schoolName, value, print);
+}
+
+async function displaySchoolValue(school) {
+  const promisified = promisify(client.GET).bind(client);
+  console.log(await promisified(school));
+  /*client.GET(school, (error, response) => {
+    console.log(response);
+  });*/
+}
+
+async function main() {
+  await displaySchoolValue('Holberton');
+  await setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+
+main();
