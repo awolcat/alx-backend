6122600d35f5e21e7b7b72e1b0a27a655a4117a4
Albert <albertirura11@gmail.com>
2024-02-11 18:25:27 +0300

    stock tracking with redis

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,13 +1,13 @@
 import { createClient } from 'redis';
-import {promisify } from 'util';
+import { promisify } from 'util';
 
 const express = require('express');
 
 const listProducts = [ 
-  { id: 1, name: 'Suitcase 250', price: 50, stock: 4 }, 
-  { id: 2, name: 'Suitcase 450', price: 100, stock: 10 },
-  { id: 3, name: 'Suitcase 650', price: 350, stock: 2 },
-  { id: 4, name: 'Suitcase 1050', price: 550, stock: 5 },
+  { id: 1, name: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 }, 
+  { id: 2, name: 'Suitcase 450', price: 100, initialAvailableQuantity: 10 },
+  { id: 3, name: 'Suitcase 650', price: 350, initialAvailableQuantity: 2 },
+  { id: 4, name: 'Suitcase 1050', price: 550, initialAvailableQuantity: 3 },
 ];
 
 function getItemById(id) {
@@ -17,8 +17,16 @@ function getItemById(id) {
 }
 
 const client = createClient();
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server:', error.toString());
+});
 
-function reserveStockById(itemID, stock) {
+client.on('connect', (err, response) => {
+  console.log('Redis client connected to the server');
+});
+
+
+function reserveStockById(itemId, stock) {
   client.SET(`item.${itemId}`, stock);
 }
 
@@ -33,16 +41,37 @@ app.get('/list_products', (request, response) => {
   response.json(listProducts);
 });
 
-app.get('/list_products/:itemId', (request, response) => {
+app.get('/list_products/:itemId', async (request, response) => {
   const id = parseInt(request.params.itemId);
   const item = getItemById(id);
   if ( !item ) {
     response.json({'status': 'Product not found'});
   } else {
+    const reserved = await getCurrentReservedStockById(item.id) ||  0;
+    const reservedStock = parseInt(reserved);
+    item.currentQuantity = item.initialAvailableQuantity - reservedStock;
     response.json(item);
   }
 });
 
+app.get('/reserve_product/:itemId', async (request, response) => {
+  const id = parseInt(request.params.itemId);
+  const item = getItemById(id);
+  if ( !item ) {
+    response.json({'status' : 'Product not found'});
+    return;
+  } else if ( item ) {
+    const reserved = await getCurrentReservedStockById(item.id) ||  0;
+    const stockReserved = parseInt(reserved);
+    item.currentQuantity = item.initialAvailableQuantity - stockReserved;
+    if ( item.currentQuantity < 1 ) {
+      response.json({'status': 'Not enough stock available', 'itemID': item.id});
+      return;
+    } else {
+      await reserveStockById(item.id, stockReserved + 1);
+      response.json({'status': 'Reservation confirmed','itemId':1, 'remaining': item.currentQuantity});
+    }
+  }
+});
+
 app.listen(port);
-
-
